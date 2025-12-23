64dc638710ea036ab264a510d5f1992f9627b3ca
sandeshkarad7472@gmail.com <sandeshkarad7472@gmail.com>
2025-12-23 23:07:47 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {nanoid} from "nanoid";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/create", (req, res) => {
+  const {url} = req.body;
+  console.log(url);
+
+  res.send({ id: nanoid(7) });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
